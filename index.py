68e0f0817508aaba747dc5fc7c5c8f9828f73c86
Mozamity <84947902+Mozamity@users.noreply.github.com>
2023-01-12 14:23:17 +0400

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world(): 
     return 'Hello World!'
+
+
+
+
+
+
+@app.route('/guide', methods=["POST"])
+def add_guide():
+    title = request.json['title']
+    content = request.json['content']
+
+    
+    guide = 'Sunny'
+
+    return jsonify(content)
 
 if __name__ == '__main__':
     app.run()
