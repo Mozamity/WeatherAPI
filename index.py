46e2564774c5448d8438498c665de51416047466
Mozamity <84947902+Mozamity@users.noreply.github.com>
2023-01-12 14:52:30 +0400

    Update index.py

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
+
 from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world(): 
-    return 'Hello Moozakeer!'
+    return 'Hello World!'
 
 
 
@@ -13,13 +17,33 @@ def hello_world():
 
 @app.route('/guide', methods=["POST"])
 def add_guide():
-    title = request.json['title']
-    content = request.json['content']
+    speed = request.json['speed']
+    gyrosx = request.json['gyrosx']
+    gyrosy = request.json['gyrosy']
+    magnitude = request.json['magnitude']
+
+
+
+    modelQA = pickle.load(open('ML/Decision-tree.pkl','rb'))
+    arr = np.array([[speed, gyrosx, gyrosy, magnitude]])
+    mlouptput = modelQA.predict(arr)
+
+    if 'ok' in mlouptput:
+        #OutputGB = 0
+        roadsurface = 'ok'
+        
+    if 'hump' in mlouptput:
+    #     #OutputGB = 2
+        roadsurface = 'hump'
+
+    if 'Pothole' in mlouptput:
+    #     #OutputGB = 1
+        roadsurface = 'Pothole'
 
     
     guide = 'Sunny'
 
-    return jsonify(content)
+    return jsonify(roadsurface)
 
 if __name__ == '__main__':
     app.run()
